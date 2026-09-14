0d8a4532bed5cc44b61c9e77d16a812cf2a162bd
Kesuda814 <u6736640@au.edu>
2026-09-14 14:31:39 +0700

    Fix user context and home component for assignment

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { UserProvider } from "./context/UserContext";
 import Home from "./Home";
 import Login from "./Login";
 import UserManagement from "./UserManagement";
+// Import other pages/components as needed
 
 export default function App() {
   return (
@@ -12,8 +13,9 @@ export default function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/" element={<Home />}>
             <Route path="user" element={<UserManagement />} />
+            {/* Add other nested routes here */}
           </Route>
-          <Route path="*" element={<Navigate to="/" />} />
+          <Route path="*" element={<Navigate to="/login" replace />} />
         </Routes>
       </BrowserRouter>
     </UserProvider>
